d4606c2eba61590008d245e5576645c752179363
Cleverson Guandalin <cleversongnd@gmail.com>
2023-05-02 13:37:45 -0300

    Aula01 - Conhecendo o Visual Studio

---
+++ DesenvolvimentoDyn365comC#/PreparandoAmbienteVisualStudioeSDK/MeuPrimeiroProjeto/MeuPrimeiroProjeto/PrimeiraClasse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuPrimeiroProjeto
+{
+    public class PrimeiraClasse
+    {
+        public void HelloWorld()
+        {
+            Console.WriteLine("Digite seu nome: ");
+            string nome = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("====================================");
+            Console.WriteLine("Hello " + nome);
+            Console.WriteLine("Precione qualquer tecla para continuar ...");
+            Console.WriteLine("====================================");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DesenvolvimentoDyn365comC#/PreparandoAmbienteVisualStudioeSDK/MeuPrimeiroProjeto/MeuPrimeiroProjeto/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuPrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrimeiraClasse primeiraClasse = new PrimeiraClasse();
+            primeiraClasse.HelloWorld();
+        }
+    }
+}
