d9507e948d5c2e19346d54abde67a812fca56612
Cleverson Guandalin <cleversongnd@gmail.com>
2023-05-11 13:20:57 -0300

    Desenvolvimento no Dynamics 365 CE com C# - Aulas

---
+++ Dyn365comC/MeuPrimeiroProjeto/MeuPrimeiroProjeto/Conexao.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuPrimeiroProjeto
+{
+    internal class Conexao
+    {
+        private static CrmServiceClient crmServiceClientTrinamento; // variável do tipo CrmServiceCliente
+
+        public CrmServiceClient ObterConexao() // método que devolve uma CrmServiceCliente
+        {
+            // variável contendo a ConnectionString
+            var connectionStringCRM = @"AuthType=OAuth;
+            Username = UserSvc1@1v0cst.onmicrosoft.com;
+            Password = Zav80536; SkipDiscovery = True;
+            AppId = 23364d1d-28e2-ed11-a80b-002248de5ce3;
+            RedirectUri = app://16b1e39a-5e1d-e40a-8bbc-69aa68e762ed;
+            Url = https://org04894b71.crm2.dynamics.com/main.aspx;";
+
+
+            if (crmServiceClientTrinamento == null) // verifica se a variável crmServiceClientTrinamento está vazia
+            {
+                System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12; // define o protocolo segurança
+                crmServiceClientTrinamento = new CrmServiceClient(connectionStringCRM); // executa a conexão
+            }
+            return crmServiceClientTrinamento; // retorna a conexão para o método chamador
+        }
+
+    }
+}
---
+++ Dyn365comC/MeuPrimeiroProjeto/MeuPrimeiroProjeto/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuPrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var crmService = new Conexao().ObterConexao();
+            FetchXML (crmService);
+        }
+
+        static void FetchXML(CrmServiceClient crmService) // recebe como parâmetro a conexão
+        {
+            string query = @"<fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='false'>
+                              <entity name='account'>
+                                <attribute name='name' />
+                                <attribute name='telephone1' />
+                                <attribute name='accountid' />
+                                <attribute name='emailaddress1' />
+                                <order attribute='name' descending='false' />
+                              </entity>
+                            </fetch>"; // declara variável do tipo String contendo o FechXML da consulta
+
+            EntityCollection colecao = crmService.RetrieveMultiple(new FetchExpression(query)); // declara variável do tipo 	            EntityCollection e executa o método RetrieveMultiple enviando como parâmetro a variável query
+
+            foreach (var item in colecao.Entities) // para cada entidade (registro)  armazenará na variável item
+            {
+                Console.WriteLine(item["name"]); // exibe o atributo name
+                if (item.Attributes.Contains("telephone1")) // verifica se retornou o atributo telephone1
+                {
+                    Console.WriteLine(item["telephone1"]); // exibe o atributo telephone
+                }
+            }
+            Console.ReadKey(); // aguarda o usuário pressionar qualquer tecla para continuar
+        }
+
+    }
+}
